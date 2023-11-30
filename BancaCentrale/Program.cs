df4305dbcfdccd153f31b5515a3dfc078ef33302
Ancuta.Cos <ancutacosovanu82@gmail.com>
2023-11-30 11:58:13 +0100

    Esercizio Banca Centrale aggiunto al repository Esercizi

---
+++ BancaCentrale/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+// Interfaccia per il controllo dei diritti umani
+interface IHumanRightsChecker
+{
+    string CheckHumanRights(string country);
+}
+
+// Classe astratta per la banca centrale
+abstract class CentralBank
+{
+    public abstract double CalculateSpread(string country);
+}
+
+// Classe EuroCentralBank che eredita da CentralBank e implementa IHumanRightsChecker
+class EuroCentralBank : CentralBank, IHumanRightsChecker
+{
+    public override double CalculateSpread(string country)
+    {
+        double baseSpread = 1.5;
+
+        if (IsEuroZoneCountry(country))
+        {
+            return baseSpread;
+        }
+        else
+        {
+            // Aggiungi logica personalizzata qui per calcolare lo spread per paesi non nell'Eurozona
+            double nonEuroZoneSpread = baseSpread + 0.5; // Esempio di calcolo personalizzato
+            return nonEuroZoneSpread;
+        }
+    }
+
+    public string CheckHumanRights(string country)
+    {
+        if (IsEuroZoneCountry(country))
+        {
+            return "Il paese dell'Eurozona rispetta i diritti umani.";
+        }
+        else if (IsEUCountry(country))
+        {
+            return "Il paese EU rispetta i diritti umani.";
+        }
+        else if (IsONUCountry(country))
+        {
+            return "Il paese ONU rispetta i diritti umani.";
+        }
+        else
+        {
+            return "Il paese non appartiene né all'Eurozona, né all'EU, né all'ONU.";
+        }
+    }
+
+    private bool IsEuroZoneCountry(string country)
+    {
+        // Implementazione fittizia per determinare se il paese fa parte dell'Eurozona
+        // In questo esempio, consideriamo solo due paesi a titolo illustrativo
+        return country.Equals("Francia") || country.Equals("Spagna");
+    }
+
+    private bool IsEUCountry(string country)
+    {
+        // Implementazione fittizia per determinare se il paese fa parte dell'EU
+        // In questo esempio, consideriamo solo due paesi a titolo illustrativo
+        return country.Equals("Italia") || country.Equals("Germania");
+    }
+
+    private bool IsONUCountry(string country)
+    {
+        // Implementazione fittizia per determinare se il paese fa parte dell'ONU
+        // In questo esempio, consideriamo solo due paesi a titolo illustrativo
+        return country.Equals("Stati Uniti") || country.Equals("Cina");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        EuroCentralBank euroBank = new EuroCentralBank();
+
+        // Esempio di calcolo dello spread per un paese
+        double spreadItaly = euroBank.CalculateSpread("Italia");
+        double spreadSpain = euroBank.CalculateSpread("Spagna");
+
+        Console.WriteLine($"Lo spread per l'Italia è: {spreadItaly}");
+        Console.WriteLine($"Lo spread per la Spagna è: {spreadSpain}");
+
+        // Esempio di controllo dei diritti umani per un paese
+        string humanRightsCheckFrance = euroBank.CheckHumanRights("Francia");
+        string humanRightsCheckGermany = euroBank.CheckHumanRights("Germania");
+        string humanRightsCheckUSA = euroBank.CheckHumanRights("Stati Uniti");
+        string humanRightsCheckUnknown = euroBank.CheckHumanRights("PaeseSconosciuto");
+
+        Console.WriteLine("\nRisultati del controllo dei diritti umani:");
+        Console.WriteLine($"Francia: {humanRightsCheckFrance}");
+        Console.WriteLine($"Germania: {humanRightsCheckGermany}");
+        Console.WriteLine($"Stati Uniti: {humanRightsCheckUSA}");
+        Console.WriteLine($"Paese Sconosciuto: {humanRightsCheckUnknown}");
+    }
+}
---
+++ NEstClass.DynamicType/Program.cs
@@ -0,0 +1,99 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace Dynamics.Lesson
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            #region Deserialize 
+            //  getObj();
+            // getDynamicObj();
+            #endregion
+
+
+
+            State italy = new State("Italy");
+            #region StateDynamic 
+            // Ritorno tutto l'oggetto State tramite Dynamic
+
+            Console.WriteLine("\n----GetStateDynamic Private Info ----\n");
+
+
+            //dynamic StateDynamic = italy.GetStateDynamic();
+
+
+            //Console.WriteLine(StateDynamic.Name);
+            //Console.WriteLine(StateDynamic.Population);
+            //Console.WriteLine(StateDynamic.Regioni[0].Name);
+            //Console.WriteLine(StateDynamic.Regioni[1].Name);
+            //Console.WriteLine(StateDynamic.Regioni[1].Province[0].Name);
+
+            Console.WriteLine("         \n----StateDynamic Private  Info----\n");
+
+            //Console.WriteLine(StateDynamic._name);
+            //Console.WriteLine(StateDynamic._population);
+            //Console.WriteLine(StateDynamic._regioni[0].Name);
+            //Console.WriteLine(StateDynamic._regioni[1].Name);
+            //Console.WriteLine(StateDynamic._regioni[1].Province[0].Name);
+
+
+
+            #endregion
+            #region PrivateInfo  
+            // Ritorno i membri PRIVATE dello State tramite Anonymous + dynamic 
+            Console.WriteLine("\n----Ritorno i membri privati dello State tramite Anonymous + dynamic----\n");
+
+            dynamic PrivateInfo = italy.GetPrivateInfoByAnonymous();
+
+            Console.WriteLine(PrivateInfo._name);
+            Console.WriteLine(PrivateInfo._population);
+            Console.WriteLine(PrivateInfo._regioni[0].Name);
+            Console.WriteLine(PrivateInfo._regioni[1].Name);
+            Console.WriteLine(PrivateInfo._regioni[1].Province[0].Name);
+            #region Considerazioni 
+            /*  Anoynmous type non aggira le regole di visibilita dell'oggetto,
+                ma crea al volo un oggetto anonimo ci chiave/valore di tipo stringa. 
+                Il suo uso però a dei LIMITI. Se non si rispetta perfettamente la struttura degli oggetti (Ese: Annidamento)
+                si possono generare bug che saranno poi rilevabili solo a Runtime.
+
+            */
+            #endregion
+
+            #endregion    
+
+
+            Console.Read();
+        }
+
+        static Person? getObj()
+        {
+            string jsonString = "{\"Name\":\"John\", \"Age\":30}"; //  Gas -> Spedito 
+            Person? person = JsonConvert.DeserializeObject<Person>(jsonString); // tipizzato
+            person = null;
+            Console.WriteLine($"Name: {person?.Name},\n Age: {person?.Age}");
+            return person;
+
+
+        }
+        static Person? getDynamicObj()
+        {
+            string jsonString = "{\"Fullname\":\"John Doe\", \"Age\":30}"; // Gas 
+            dynamic? person = JsonConvert.DeserializeObject(jsonString); //  utilizza tipizzato ma Dinamico
+            Console.WriteLine($"Name: {person?.Name ?? person?.Fullname ?? "Ciao"}, Age: {person?.Age}");  // Coalescing Operators
+            return person;
+        }
+    }
+    class Person
+    {
+        public string? Name { get; set; }
+        public int? Age { get; set; }
+
+    }
+
+
+}
+
+
