ebe3099ff3e2359859f5814920fb6d0b1a853c0f
Ancuta.Cos <ancutacosovanu82@gmail.com>
2023-11-30 12:06:14 +0100

    EU_Request

---
+++ EU_Request/Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+
+// Interfacce
+public interface IPoliticalOrganization
+{
+    // Definire le funzioni per le organizzazioni politiche
+}
+
+public interface IONU
+{
+    void TerritoryDefense();
+    void PopulationControl();
+}
+
+public interface IStrasbourgCourt : IONU
+{
+    // Definire funzioni specifiche per il tribunale di Strasburgo
+}
+
+public interface INATO
+{
+    void SpesaMilitare();
+}
+
+public interface IIUnioneEuropea
+{
+    void EMA();
+    void ConstitutionIntegration();
+    void HumanRightsTribunal();
+}
+
+public interface IEuroZone
+{
+    void MonetaUnica();
+}
+
+public interface IEntitaAmministrativa
+{
+    void HNS();
+    void LAWSystem();
+    void EducationalSystem();
+    void MoveToAnotherEntity(IUEEntitaAmministrativa nuovaEntita);
+}
+
+public interface IUEParlament
+{
+    void ApproveAction(IEntitaAmministrativa entitaAmministrativa);
+}
+
+public interface IUEEntitaAmministrativa
+{
+    // Definire le proprietà e i metodi comuni a tutte le entità amministrative dell'UE
+    string Paese { get; set; }
+}
+
+// Classi astratte
+public abstract class UECitizenPublicService
+{
+    public abstract void WellfareServices();
+    public abstract void HNS(string euID);
+    public abstract void EducationalSystem(string euID);
+}
+
+public abstract class UEPublicAdministration
+{
+    public abstract void WellfareServices();
+    public abstract void HNS(string euID);
+    public abstract void EducationalSystem(string euID);
+}
+
+public class EUParlament : IUEParlament
+{
+    public void ApproveAction(IEntitaAmministrativa entitaAmministrativa)
+    {
+        Console.WriteLine($"Azione approvata dal parlamento europeo per l'entità {entitaAmministrativa.GetType().Name}");
+    }
+}
+
+public class EURegione : UEEntitaAmministrativa, IEntitaAmministrativa
+{
+    public void HNS()
+    {
+        Console.WriteLine("Implementazione di HNS per la regione");
+    }
+
+    public void LAWSystem()
+    {
+        Console.WriteLine("Implementazione di LAW System per la regione");
+    }
+
+    public void EducationalSystem()
+    {
+        Console.WriteLine("Implementazione di Educational System per la regione");
+    }
+
+    public void RequestHealthSSN()
+    {
+        Console.WriteLine("Richiesta Health SSN dalla regione");
+    }
+}
+
+public class EUProvince : UEEntitaAmministrativa, IEntitaAmministrativa
+{
+    public void HNS()
+    {
+        Console.WriteLine("Implementazione di HNS per la provincia");
+    }
+
+    public void LAWSystem()
+    {
+        Console.WriteLine("Implementazione di LAW System per la provincia");
+    }
+
+    public void EducationalSystem()
+    {
+        Console.WriteLine("Implementazione di Educational System per la provincia");
+    }
+
+    public void MoveToAnotherEntity(IUEEntitaAmministrativa nuovaEntita)
+    {
+        Console.WriteLine($"Prima del cambio - Paese: {this.Paese}");
+        // Aggiorna la proprietà Paese qui, se necessario
+        Console.WriteLine($"Dopo il cambio - Nuovo Paese: {nuovaEntita.Paese}");
+    }
+}
+
+
+
+public abstract class UEEntitaAmministrativa : IUEEntitaAmministrativa
+{
+    public string Paese { get; set; }
+
+    public virtual void MoveToAnotherEntity(IUEEntitaAmministrativa nuovaEntita)
+    {
+        // Logica di base per lo spostamento
+        Console.WriteLine($"Prima del cambio - Paese: {this.Paese}");
+        this.Paese = nuovaEntita.Paese;
+        Console.WriteLine($"Dopo il cambio - Nuovo Paese: {this.Paese}");
+    }
+
+
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Esempio di utilizzo e chiamata di funzioni
+        EURegione euRegione = new EURegione();
+        EUProvince euProvince = new EUProvince { Paese = "Italia" }; // Aggiungi qui le proprietà necessarie
+
+        // Esempio di spostamento di un'entità territoriale
+        euRegione.MoveToAnotherEntity(euProvince);
+
+
+    }
+}
